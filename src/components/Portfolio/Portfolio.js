38121cd18d756ab6fd43b6d581fe04291e94af6c
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-05-14 17:03:19 +0200

    Work on technologies section and concept of portfolio section

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -10,13 +10,24 @@ export class Portfolio extends React.Component {
                 </h1>
                 <div class='container Portfolio-projects'>
                     <div class='row Portfolio-projects--line'>
-                        <div class='col-5 Portfolio-projects--line-element'>X</div>
+                        <div class='col-3 Portfolio-projects--line-text'>askjhd asjdhaskjd skjdhad kjsahdkjsahd jashdkjsa</div>
+                        <div class='col-2 Portfolio-projects--line-element bg-1'></div>
                         <div class='col-2 Portfolio-projects--line-vertical'></div>
                         <div class='col-5'>z</div>
                     </div>
                     <div class="row">
                         <div class="col-5">X</div>
-                        <div class="col-2"></div>
+                        <div class="col-2 Portfolio-projects--line-vertical"></div>
+                        <div class="col-5">z</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-5">X</div>
+                        <div class="col-2 Portfolio-projects--line-vertical"></div>
+                        <div class="col-5">z</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-5">X</div>
+                        <div class="col-2 Portfolio-projects--line-vertical"></div>
                         <div class="col-5">z</div>
                     </div>
                 </div>
