aabd719d225e28713c02b3d073abb4f6be9db328
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-05-15 18:26:10 +0200

    Work on portfolio concept

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,33 +2,63 @@ import React from 'react';
 import './Portfolio.scss';
 
 export class Portfolio extends React.Component {
+    createLine() {
+    }
+
     render() {
         return (
             <div id='Portfolio' className='Portfolio'>
                 <h1 className='Portfolio-header'>
                     Portfolio
                 </h1>
-                <div class='container Portfolio-projects'>
-                    <div class='row Portfolio-projects--line'>
-                        <div class='col-3 Portfolio-projects--line-text'>askjhd asjdhaskjd skjdhad kjsahdkjsahd jashdkjsa</div>
-                        <div class='col-2 Portfolio-projects--line-element bg-1'></div>
-                        <div class='col-2 Portfolio-projects--line-vertical'></div>
-                        <div class='col-5'>z</div>
+                <div className='container Portfolio-projects'>
+                    <div className='row Portfolio-projects--line'>
+                        <div className='col-6 bordered'>
+                        {/* project 1 */}
+                            <div className='row'>
+                                <div className='col-8 Portfolio-projects--line-text'><p className="Portfolio-projects--line-text-header">Golden project</p><p>askjhd asjdhaskjd skjdhad kjsahdkjsahd jashdkjsa</p></div>
+                                <div className='col-4 Portfolio-projects--line-element bg-1'></div>
+                            </div>
+                        </div>
+                        <div className='col-6 hgt moved'>
+                        {/* project 2 */}
+                            <div className="row">
+                                <div className='col-4 Portfolio-projects--line-element bg-2 moved'></div>
+                                <div className='col-8 Portfolio-projects--line-text moved'><p className="Portfolio-projects--line-text-header">Golden project</p><p>askjhd asjdhaskjd skjdhad kjsahdkjsahd jashdkjsa</p></div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="row">
-                        <div class="col-5">X</div>
-                        <div class="col-2 Portfolio-projects--line-vertical"></div>
-                        <div class="col-5">z</div>
+                    <div className='row Portfolio-projects--line'>
+                        <div className='col-6 bordered'>
+                        {/* project 3 */}
+                            <div className='row'>
+                                <div className='col-8 Portfolio-projects--line-text'><p className="Portfolio-projects--line-text-header">Golden project</p><p>askjhd asjdhaskjd skjdhad kjsahdkjsahd jashdkjsa</p></div>
+                                <div className='col-4 Portfolio-projects--line-element bg-3'></div>
+                            </div>
+                        </div>
+                        <div className='col-6 hgt moved'>
+                        {/* project 4 */}
+                            <div className="row">
+                                <div className='col-4 Portfolio-projects--line-element bg-4 moved'></div>
+                                <div className='col-8 Portfolio-projects--line-text moved'><p className="Portfolio-projects--line-text-header">Golden project</p><p>askjhd asjdhaskjd skjdhad kjsahdkjsahd jashdkjsa</p></div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="row">
-                        <div class="col-5">X</div>
-                        <div class="col-2 Portfolio-projects--line-vertical"></div>
-                        <div class="col-5">z</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-5">X</div>
-                        <div class="col-2 Portfolio-projects--line-vertical"></div>
-                        <div class="col-5">z</div>
+                    <div className='row Portfolio-projects--line'>
+                        <div className='col-6 bordered'>
+                        {/* project 5 */}
+                            <div className='row'>
+                                <div className='col-8 Portfolio-projects--line-text'><p className="Portfolio-projects--line-text-header">Golden project</p><p>askjhd asjdhaskjd skjdhad kjsahdkjsahd jashdkjsa</p></div>
+                                <div className='col-4 Portfolio-projects--line-element bg-1'></div>
+                            </div>
+                        </div>
+                        <div className='col-6 hgt moved'>
+                        {/* project 6 */}
+                            <div className="row">
+                                <div className='col-4 Portfolio-projects--line-element bg-1 moved'></div>
+                                <div className='col-8 Portfolio-projects--line-text moved'><p className="Portfolio-projects--line-text-header">Golden project</p><p>askjhd asjdhaskjd skjdhad kjsahdkjsahd jashdkjsa</p></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
