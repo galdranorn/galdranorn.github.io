b2101abbe571a9b993265c26880370d4e080fa31
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-06-12 18:40:09 +0200

    Updates

---
+++ precache-manifest.183d9f069d581d43080d1b039cf6cd64.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "469c82ed32000fb85c3b186787f9db85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2384c96c6f639686795",
+    "url": "/static/css/2.87c3ea4e.chunk.css"
+  },
+  {
+    "revision": "1ce1cb6a957f735a391d",
+    "url": "/static/css/main.c5386188.chunk.css"
+  },
+  {
+    "revision": "a2384c96c6f639686795",
+    "url": "/static/js/2.59bb5830.chunk.js"
+  },
+  {
+    "revision": "1ce1cb6a957f735a391d",
+    "url": "/static/js/main.920ee624.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7a797c5f77b1d72b7d9999b2af7e7cf4",
+    "url": "/static/media/10node.7a797c5f.png"
+  },
+  {
+    "revision": "1a63df42cd9188b7a1cafbfef03f6473",
+    "url": "/static/media/13react.1a63df42.png"
+  },
+  {
+    "revision": "c33e1e103ac1886bd16947a484c8c469",
+    "url": "/static/media/14redux.c33e1e10.png"
+  },
+  {
+    "revision": "664a8c00ef526bf33edf55808601d461",
+    "url": "/static/media/1html.664a8c00.png"
+  },
+  {
+    "revision": "c4ff598f880f65385e9e40e64bd8ff71",
+    "url": "/static/media/2css.c4ff598f.png"
+  },
+  {
+    "revision": "d1d7a23aaf0db8bb7d02e2494d6ec58b",
+    "url": "/static/media/5bootstrap.d1d7a23a.png"
+  },
+  {
+    "revision": "019e679a6d512c746e47b0c5e1faedea",
+    "url": "/static/media/6sass.019e679a.png"
+  },
+  {
+    "revision": "66dd866fcc53a3ac6fc62852256274e1",
+    "url": "/static/media/7jquery.66dd866f.png"
+  },
+  {
+    "revision": "c7e0d04fce8b8e63f5d9b6903168b921",
+    "url": "/static/media/8api.c7e0d04f.png"
+  },
+  {
+    "revision": "04d40c2e2483ce55c6de3e47cc9b041a",
+    "url": "/static/media/9ajax.04d40c2e.png"
+  },
+  {
+    "revision": "0f952e339f94bd75788d325d55b7771a",
+    "url": "/static/media/about.0f952e33.jpg"
+  },
+  {
+    "revision": "7616ae81c958dbb081047c8d97b0e9e9",
+    "url": "/static/media/contact.7616ae81.jpg"
+  },
+  {
+    "revision": "e6a7dff00ff40c2f6e75f10fb97d47dd",
+    "url": "/static/media/monstera-left.e6a7dff0.png"
+  },
+  {
+    "revision": "dc2aedfc58ebbcda030d5c8c3c21e009",
+    "url": "/static/media/monstera-right.dc2aedfc.png"
+  },
+  {
+    "revision": "790c8a67c16d6e25a2a8f0762dba8a06",
+    "url": "/static/media/monstera.790c8a67.jpg"
+  },
+  {
+    "revision": "2ad62e938f44718391a753b6e81a2f7b",
+    "url": "/static/media/project-1.2ad62e93.png"
+  },
+  {
+    "revision": "cc7ed72f8f43d06d9cae04999149583e",
+    "url": "/static/media/project-3.cc7ed72f.png"
+  },
+  {
+    "revision": "5d7b84527739aee4deabe9317b6c36fa",
+    "url": "/static/media/project-4.5d7b8452.png"
+  },
+  {
+    "revision": "e4d94109d6c176e2f14ac8cdfa051b68",
+    "url": "/static/media/project10.e4d94109.png"
+  },
+  {
+    "revision": "f06fb2f6c5fae9211bcadc0dd15955be",
+    "url": "/static/media/project11.f06fb2f6.png"
+  },
+  {
+    "revision": "a7b0a10aec8df095e654630ac57c39a3",
+    "url": "/static/media/project12.a7b0a10a.png"
+  },
+  {
+    "revision": "38aad51424e7f56ea957ad09a26885fe",
+    "url": "/static/media/project13.38aad514.png"
+  },
+  {
+    "revision": "f55281b60b43b169426cd5c29050cd85",
+    "url": "/static/media/project14.f55281b6.png"
+  },
+  {
+    "revision": "0419ad6f76c35a331141846c96822c91",
+    "url": "/static/media/project15.0419ad6f.png"
+  },
+  {
+    "revision": "01d26b2a8bae50f171dd317f77ae38c6",
+    "url": "/static/media/project2.01d26b2a.png"
+  },
+  {
+    "revision": "3969c50ae7671ae3f640771ba473a429",
+    "url": "/static/media/project5.3969c50a.png"
+  },
+  {
+    "revision": "f629d2dcbe0fe193fe2b7f22d6dffcb9",
+    "url": "/static/media/project6.f629d2dc.png"
+  },
+  {
+    "revision": "28942d2622d32c2bdc2b7f4ac1b2c2e0",
+    "url": "/static/media/project7.28942d26.png"
+  },
+  {
+    "revision": "d978dcaedb35d490e47fc445361f95ef",
+    "url": "/static/media/project8.d978dcae.png"
+  },
+  {
+    "revision": "2645902f2f86c9f0f7af321ecfab3fb0",
+    "url": "/static/media/project9.2645902f.png"
+  }
+]);
--- src/actions/locale.js
+++
@@ -1,6 +0,0 @@
-export const setLanguage = (val) => {
-    return {
-        type: "SET_LANGUAGE",
-        payload: val
-    }
-};
